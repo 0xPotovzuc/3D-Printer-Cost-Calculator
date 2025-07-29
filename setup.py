d84b28c16d1ea3a1902e4fe18fe408086d67208d
0xPotovzuc <hasturkmetehan@gmail.com>
2025-07-29 17:09:05 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,18 @@ base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
+# MSI yükleyicisi için seçenekler
+bdist_msi_options = {
+    "add_to_path": False,
+    "initial_target_dir": r"%ProgramFiles%\MaliyetHesaplayici",
+    "all_users": True,
+}
+
 setup(
-    name="Maliyet Hesaplayıcı",
+    # DEĞİŞTİRİLDİ: Türkçe karakter sorunlarını önlemek için isim basitleştirildi.
+    name="MaliyetHesaplayici",
     version="3.0",
     description="3D Baskı Maliyet & Kar Hesaplayıcı Pro",
+    options={"bdist_msi": bdist_msi_options},
     executables=[Executable("MaliyetV3.py", base=base, target_name="Maliyet Hesaplayıcı.exe")]
 )
